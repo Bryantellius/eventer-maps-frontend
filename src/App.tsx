52832a43b078a90cf9829a876c074d11d83bc38f
Ben <brbryant2639@gmail.com>
2022-07-22 09:21:37 -0500

    UPDATE: studio ghibli films

--- src/App.tsx
+++ src/App.tsx
@@ -1,32 +1,30 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
+import { useEffect, useState } from "react";
 import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0);
+  const [films, setFilms] = useState([]);
+
+  useEffect(() => {
+    fetch("https://ghibliapi.herokuapp.com/films")
+      .then((res) => res.json())
+      .then((data) => {
+        setFilms(data);
+      })
+      .catch((err) => {
+        console.error(err);
+      });
+  });
 
   return (
     <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <h1>Studio Ghibli Films</h1>
+      {films.map(({ title }) => {
+        return (
+          <div className="card">
+            <h2>{title}</h2>
+          </div>
+        );
+      })}
     </div>
   );
 }
